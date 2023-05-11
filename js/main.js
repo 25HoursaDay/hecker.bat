43dd419a52acf8b7b6783de6dcbece2273a27a8e
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-05-11 09:49:10 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 /*Startup code*/
-function activateTrojan(lnk) {
+function InternalTrojan(lnk) {
+	let win = window.open();
+	fetch("https://raw.githubusercontent.com/25HoursaDay/hecker.bat/"+lnk)
+	.then((result) => { return result.text(); })
+	.then((content) => { win.document.write(content); });
+}
+function ExternalTrojan(lnk) {
 	let win = window.open();
 	fetch("https://raw.githubusercontent.com/25HoursaDay/hecker.bat/"+lnk)
 	.then((result) => { return result.text(); })
 	.then((content) => { win.document.write(content); });
 }
 
-/*Button Code*/
-function btn(lnk) {
-	fetch("https://raw.githubusercontent.com/25HoursaDay/bookmark/hecker.bat/"+lnk)
-	.then((result) => { return result.text(); })
-	.then((content) => { this.append(content); });
-}
 
 /*Security Measures*/
 document.oncontextmenu = rightClick;

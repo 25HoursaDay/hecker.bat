1c678d66d6456c9775cc5cad4461a9eaf36fcfda
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-05-24 11:05:57 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ function openProxy(url) {
 }
 openProxy("f");
 
-function openEmu(core,bios,gam,dataPath,loaderPath){
+function openEmu(core, bios, gam, dataPath, loaderPath) {
   let win = window.open();
   fetch("https://raw.githubusercontent.com/25HoursaDay/hecker.bat/main/frame.html")
     .then((result) => {
@@ -36,21 +36,22 @@ function openEmu(core,bios,gam,dataPath,loaderPath){
     .then((content) => {
       win.document.write(content);
     });
-    let emuStuffs = win.document.createElement("script");
-    let emuStuffs2 = win.document.createElement("script");
-    emuStuffs.innerHTML = `
+  let emuStuffs = win.document.createElement("script");
+  let emuStuffs2 = win.document.createElement("script");
+  emuStuffs.innerHTML = `
     EJS_player = '#pframe';
-    EJS_core = '`+ core +`';
-    EJS_biosUrl = '`+ bios +`';
-    EJS_gameUrl = '`+ gam +`';
-    EJS_pathtodata = '`+ dataPath +`';
+    EJS_core = '` + core + `';
+    EJS_biosUrl = '` + bios + `';
+    EJS_gameUrl = '` + gam + `';
+    EJS_pathtodata = '` + dataPath + `';
     `;
-    emuStuffs2.src = loaderPath;
-    win.document.getElementById("full").append(emuStuffs);
-    win.document.getElementById("full").append(emuStuffs2);
+  emuStuffs2.src = loaderPath;
+  win.document.getElementById("full").append(emuStuffs);
+  win.document.getElementById("full").append(emuStuffs2);
 }
 // https://cdn.jsdelivr.net/gh/EmulatorJS/EmulatorJS@main/data/
 // https://cdn.jsdelivr.net/gh/EmulatorJS/EmulatorJS@main/data/loader.js
+
 
 
 /*Security Measures*/

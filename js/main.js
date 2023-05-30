8c032fca8358e9ba94fdd6dff7da547cf425c164
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-05-30 08:29:04 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -53,22 +53,74 @@ function openEmu(core, bios, gam, dataPath, loaderPath) {
 // https://cdn.jsdelivr.net/gh/EmulatorJS/EmulatorJS@main/data/loader.js
 
 if (HeckerPage == "ruffle") {
-let ruffleSrc = document.createElement("script");
-let ruffleSrc2 = document.createElement("script");
-ruffleSrc.src = "https://unpkg.com/@ruffle-rs/ruffle";
-ruffleSrc2.src = "https://cdn.jsdelivr.net/gh/25HoursaDay/hecker.bat@main/js/ruffle.js";
-document.getElementById("full").append(ruffleSrc);
-document.getElementById("full").append(ruffleSrc);
+  let ruffleSrc = document.createElement("script");
+  let ruffleSrc2 = document.createElement("script");
+  ruffleSrc.src = "https://unpkg.com/@ruffle-rs/ruffle";
+  ruffleSrc2.src = "https://cdn.jsdelivr.net/gh/25HoursaDay/hecker.bat@main/js/ruffle.js";
+  document.getElementById("full").append(ruffleSrc);
+  document.getElementById("full").append(ruffleSrc);
 }
 
 
 function fullscreenscreen(elem) {
-        elem.requestFullscreen();
-      }
+  elem.requestFullscreen();
+}
 
-      function closeScreen() {
-        document.getElementById("full").remove();
-      }
+function closeScreen() {
+  document.getElementById("full").remove();
+}
+
+function minimize() {
+  document.getElementById("efef").style.display = "none";
+  document.getElementById("full").style.height = "36px";
+  document.getElementById("minimize").style.display = "none";
+  document.getElementById("maximize").style.display = "block";
+}
+
+function maximize() {
+  document.getElementById("efef").style.display = "flex";
+  document.getElementById("full").style.height = "75vh";
+  document.getElementById("maximize").style.display = "none";
+  document.getElementById("minimize").style.display = "block";
+}
+dragElement(document.getElementById("full"));
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  elmnt.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
 /*Security Measures*/
 document.oncontextmenu = rightClick;
-function rightClick(clickEvent) {clickEvent.preventDefault();}  
+
+function rightClick(clickEvent) {
+  clickEvent.preventDefault();
+}
